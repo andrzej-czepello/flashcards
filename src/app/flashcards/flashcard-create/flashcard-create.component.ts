b0fddf47a9470e0556a5215f7327c4ea63ffe715
andrzej-czepello <czepello@gmail.com>
2020-06-23 20:58:31 +0200

    css changes

--- src/app/flashcards/flashcard-create/flashcard-create.component.ts
+++ src/app/flashcards/flashcard-create/flashcard-create.component.ts
@@ -1,6 +1,7 @@
 import { FlashcardsService } from './../flashcard.service';
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-flashcard-create',
@@ -11,7 +12,7 @@ export class FlashcardCreateComponent {
   enteredTitle = '';
   enteredContent = '';
 
-  constructor(public flashcardsService: FlashcardsService) { }
+  constructor(public flashcardsService: FlashcardsService, private snackBar: MatSnackBar) { }
 
   onAddFlashcard(form: NgForm) {
     if (form.invalid) {
@@ -20,6 +21,10 @@ export class FlashcardCreateComponent {
 
     this.flashcardsService.addFlashcard(form.value.title, form.value.content, '');
     form.resetForm();
+    this.snackBar.open('Flashcard created!', 'Close', {
+      duration: 2000,
+      panelClass: 'snackBar'
+    });
   }
 }
 
